d2c16f7ee325e803c8ce0387c96b6766cad1dec8
Zuka Gobronidze <zuka.gobronidze@hotmail.com>
2024-03-21 13:58:23 +0400

    succesfully migration on MSSQL database for tests

--- Ecommerce.Repository/EcommerceDbContext.cs
+++ Ecommerce.Repository/EcommerceDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce.DTO;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Repository;
 
-public class EcommerceDbContext : DbContext
+public class EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : DbContext(options)
 {
-
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<City> Cities { get; set; }
 }
---
+++ Ecommerce.Repository/UnitOfWork.cs
@@ -0,0 +1,99 @@
+﻿using Ecommerce.Facade.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IDbContextTransaction? _transaction;
+    private readonly EcommerceDbContext _context;
+    private readonly ILogger<UnitOfWork> _logger;
+    private readonly ICountryRepository _countryRepository;
+    private readonly ICityRepository _cityRepository;
+
+    public UnitOfWork(EcommerceDbContext context, ILogger<UnitOfWork> logger)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _countryRepository = new CountryRepository(context);
+        _cityRepository = new CityRepository(context);
+    }
+
+    public ICountryRepository CountryRepository => _countryRepository;
+    public ICityRepository CityRepository => _cityRepository;
+
+    public void BeginTransaction()
+    {
+        try
+        {
+            _transaction = _context.Database.BeginTransaction();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to begin transaction");
+            throw;
+        }
+    }
+
+    public void Commit()
+    {
+        try
+        {
+            _transaction?.Commit();
+            _transaction?.Dispose();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to commit transaction");
+            throw;
+        }
+    }
+
+    public void Rollback()
+    {
+        _transaction?.Rollback();
+        _transaction?.Dispose();
+    }
+
+    public void SaveChanges()
+    {
+        try
+        {
+            _context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "DbContext error");
+            throw;
+        }
+
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            _transaction?.Rollback();
+            _transaction?.Dispose();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to rollback transaction");
+            throw;
+        }
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "DbContext error");
+            throw;
+        }
+    }
+}
