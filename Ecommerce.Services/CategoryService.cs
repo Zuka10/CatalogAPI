b821104d438afdf5c8009d1a457af4932fe1251d
Zuka Gobronidze <zuka.gobronidze@hotmail.com>
2024-04-25 17:40:52 +0400

    modified services from uof to context

--- Ecommerce.Services/CategoryService.cs
+++ Ecommerce.Services/CategoryService.cs
@@ -1,91 +1,52 @@
 ﻿using Ecommerce.DTO;
-using Ecommerce.Facade.Repositories;
 using Ecommerce.Facade.Services;
 using Ecommerce.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Service;
 
-public class CategoryService(IUnitOfWork unitOfWork, EcommerceDbContext context) : ICategoryService
+public class CategoryService(EcommerceDbContext context) : ICategoryService
 {
-    private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly EcommerceDbContext _context = context;
-
-    public void Add(Category category)
-    {
-        _unitOfWork.CategoryRepository.Insert(category);
-        _unitOfWork.SaveChanges();
-    }
 
     public async Task AddAsync(Category category)
     {
-        await _unitOfWork.CategoryRepository.InsertAsync(category);
-        await _unitOfWork.SaveChangesAsync();
-    }
-
-    public void Update(int id, Category category)
-    {
-        var existingCategory = _unitOfWork.CategoryRepository.Get(id);
-        if (existingCategory is not null)
-        {
-            existingCategory.Name = category.Name;
-
-            _unitOfWork.CategoryRepository.Update(existingCategory);
-            _unitOfWork.SaveChanges();
-        }
+        await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
     }
 
     public async Task<bool> UpdateAsync(int id, Category category)
     {
-        var existingCategory = await _unitOfWork.CategoryRepository.GetAsync(id);
+        var existingCategory = await _context.Categories.FindAsync(id);
         if (existingCategory is not null)
         {
             existingCategory.Name = category.Name;
-            await _unitOfWork.CategoryRepository.UpdateAsync(existingCategory);
-            await _unitOfWork.SaveChangesAsync();
+            _context.Categories.Update(existingCategory);
+            await _context.SaveChangesAsync();
             return true;
         }
         return false;
-    }
-
-    public void Delete(int id)
-    {
-        var category = _unitOfWork.CategoryRepository.Get(id);
-        if (category is not null)
-        {
-            _unitOfWork.CategoryRepository.Delete(category);
-            _unitOfWork.SaveChanges();
-        }
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
-        var category = await _unitOfWork.CategoryRepository.GetAsync(id);
+        var category = await _context.Categories.FindAsync(id);
         if (category is not null)
         {
-            await _unitOfWork.CategoryRepository.DeleteAsync(category);
-            await _unitOfWork.SaveChangesAsync();
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
             return true;
         }
         return false;
     }
 
-    public IEnumerable<Category> GetAll()
-    {
-        return _unitOfWork.CategoryRepository.GetAll();
-    }
-
     public async Task<IEnumerable<Category>> GetAllAsync()
     {
-        return await _unitOfWork.CategoryRepository.GetAllAsync();
-    }
-
-    public Category GetById(int id)
-    {
-        return _unitOfWork.CategoryRepository.Get(id);
+        return await _context.Categories.ToListAsync();
     }
 
     public async Task<Category> GetByIdAsync(int id)
     {
-        return await _unitOfWork.CategoryRepository.GetAsync(id);
+        return await _context.Categories.FindAsync(id);
     }
 }
--- Ecommerce.Services/ProductService.cs
+++ Ecommerce.Services/ProductService.cs
@@ -1,32 +1,19 @@
 ﻿using Ecommerce.DTO;
-using Ecommerce.Facade.Repositories;
 using Ecommerce.Facade.Services;
 using Ecommerce.Repository;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Service;
 
-public class ProductService(IUnitOfWork unitOfWork, EcommerceDbContext context) : IProductService
+public class ProductService(EcommerceDbContext context) : IProductService
 {
-    private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly EcommerceDbContext _context = context;
-
-    public void Add(Product product)
-    {
-        _unitOfWork.ProductRepository.Insert(product);
-        _unitOfWork.SaveChanges();
-    }
 
     public async Task AddAsync(Product product)
     {
         ArgumentNullException.ThrowIfNull(product);
-        await _unitOfWork.ProductRepository.InsertAsync(product);
-        await _unitOfWork.SaveChangesAsync();
-    }
-
-    public void Update(Product product)
-    {
-        _unitOfWork.ProductRepository.Update(product);
+        await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
     }
 
     public async Task<bool> UpdateAsync(int id, Product product)
@@ -42,7 +29,7 @@ public class ProductService(IUnitOfWork unitOfWork, EcommerceDbContext context) 
             foreach (var existingImage in existingProduct.Images.ToList())
             {
                 // Delete from database
-                _unitOfWork.ImageRepository.Delete(existingImage);
+                _context.Images.Remove(existingImage);
                 // Delete from wwwroot/images directory
                 var filePath = Path.Combine("wwwroot/", existingImage.ImageUrl);
                 if (File.Exists(filePath))
@@ -61,39 +48,23 @@ public class ProductService(IUnitOfWork unitOfWork, EcommerceDbContext context) 
                 }
             }
 
-            await _unitOfWork.ProductRepository.UpdateAsync(existingProduct);
-            await _unitOfWork.SaveChangesAsync();
+            _context.Products.Update(existingProduct);
+            await _context.SaveChangesAsync();
             return true;
         }
         return false;
-    }
-
-
-    public void Delete(int id)
-    {
-        var product = _unitOfWork.ProductRepository.Get(id);
-        if (product is not null)
-        {
-            _unitOfWork.ProductRepository.Delete(product);
-            _unitOfWork.SaveChanges();
-        }
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
-        var product = await _unitOfWork.ProductRepository.GetAsync(id);
+        var product = await _context.Products.FindAsync(id);
         if (product is not null)
         {
-            await _unitOfWork.ProductRepository.DeleteAsync(product);
-            await _unitOfWork.SaveChangesAsync();
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
             return true;
         }
         return false;
-    }
-
-    public IEnumerable<Product> GetAll()
-    {
-        return _unitOfWork.ProductRepository.GetAll();
     }
 
     public async Task<IEnumerable<Product>> GetAllAsync()
@@ -103,11 +74,6 @@ public class ProductService(IUnitOfWork unitOfWork, EcommerceDbContext context) 
             .Include(p => p.Category)
             .Include(p => p.Images)
             .ToListAsync();
-    }
-
-    public Product GetById(int id)
-    {
-        return _unitOfWork.ProductRepository.Get(id);
     }
 
     public async Task<Product> GetByIdAsync(int id)
