e48570109066dcd9567e9ab0d6aefa83d4ac2023
Zuka Gobronidze <zuka.gobronidze@hotmail.com>
2024-06-04 20:00:11 +0400

    added AutoMapper to map gRPC DTO s to actual entities

---
+++ Catalog.gRPCService/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Ecommerce.DTO;
+
+namespace Catalog.gRPCService;
+
+public static class MapperConfig
+{
+    public static Mapper InitializeAutoMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Category, CategoryDTO>();
+            cfg.CreateMap<CategoryDTO, Category>()
+                .ForMember(c => c.Products, option => option.Ignore());
+        });
+
+        var mapper = new Mapper(config);
+        return mapper;
+    }
+}
